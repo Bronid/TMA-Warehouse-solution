0966c697c75e5162bb7c9fe06049a449de8253d5
Bronid <bronidgames@gmail.com>
2024-03-19 01:49:49 +0100

    HomeController index, list of items view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TMA_Warehouse_solution.Models;
+using TMA_Warehouse_solution.Models.Database;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace TMA_Warehouse_solution.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var items = _context.itemModels
+                .Include(i => i.ItemGroup)
+                .Include(i => i.Measurement)
+                .Include(i => i.ContactPerson)
+                .ToList();
+
+            return View(model:items);
         }
 
         public IActionResult Privacy()
